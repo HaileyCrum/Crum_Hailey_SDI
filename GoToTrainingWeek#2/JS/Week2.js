62ee9dc20a9f0dfd8dd12b646409c7f4bda30df8
hdcrum <hailey@Haileys-MacBook-Pro.local>
2015-09-04 18:31:45 -0400

    Prompted User for name
    Prompted user for name, and alerted user hello.

--- GoToTrainingWeek#2/JS/Week2.js
+++ GoToTrainingWeek#2/JS/Week2.js
@@ -4,3 +4,13 @@ SDI Section 01
 GoTo Training Week #2
 09-4-2015
  */
+
+//alert("Test to see if connected.");
+
+//Create an age Calculator
+
+//Ask the user for their name
+var name = prompt("Let's figure out how old you are!\n Please type in your Name.");
+//Use the variable and say Hello
+alert("Welcome "+name+"!");
+
