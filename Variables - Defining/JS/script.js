3d2cd50491d17378fa2bf3246b83e44c4eebd84b
hdcrum <hailey@Haileys-MacBook-Pro.local>
2015-09-04 19:55:22 -0400

    Following along with screencast
    Following along with screencast string and boolean values

--- Variables - Defining/JS/script.js
+++ Variables - Defining/JS/script.js
@@ -9,3 +9,12 @@ age = 38; //definition
 //storage container = value
 
 var yearofBirth = 1970; //declaring aand defining on the same line
+
+var age = 38; //Number variable
+
+var isStudent = false; //Boolean Variable
+
+
+var firstName = "Hailey";
+
+var phrase = 'I don\'t know \n You\'ll have to speak to the professor';
