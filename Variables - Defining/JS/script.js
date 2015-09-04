d4264ded11b9c079d70b993eb544fa32cce61814
hdcrum <hailey@Haileys-MacBook-Pro.local>
2015-09-04 19:47:39 -0400

    Following along with screencast
    Following along with screencast defining variable

---
+++ Variables - Defining/JS/script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by hailey on 9/4/15.
+ */
+//Defining Variables
+
+var age; //declaration
+age = 38; //definition
+
+//storage container = value
+
+var yearofBirth = 1970; //declaring aand defining on the same line
