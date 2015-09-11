acc49e43d888d9f4f181c8a58b14a01104601e27
hdcrum <hailey@Haileys-MacBook-Pro.local>
2015-09-10 23:43:14 -0400

    Fixed Spelling and Minor Errors
    Went back through checking for errors and spelling.

--- Crum_Hailey_Expression_Assignment/JS/script.js
+++ Crum_Hailey_Expression_Assignment/JS/script.js
@@ -8,32 +8,33 @@ alert("Hello, I am going to help you find out how much it will cost for all the 
 
 //Prompting the user how many students are in each grade and printing it out in the console
 var firstGraders =prompt("How many first graders?");
-console.log(firstGraders);
+console.log("Number of first graders "+
+firstGraders +".");
 var secondGraders =prompt("How many second graders?");
-console.log(secondGraders);
+console.log("Number of Second graders :"+ secondGraders +".");
 var thirdGraders = prompt("How many third graders?");
-console.log(thirdGraders);
+console.log("Number of third graders :"+ thirdGraders +".");
 var fourthGraders =prompt("How many fourth graders?");
-console.log(fourthGraders);
+console.log("Number of fourth graders :"+ fourthGraders +".");
 var fifthGraders = prompt("How many fifth graders?");
-console.log(fifthGraders);
+console.log("Number of fifth graders :"+ fifthGraders +".");
 var sixthGraders = prompt("How many sixth graders?");
-console.log(sixthGraders);
+console.log("Number of sixth graders :"+ sixthGraders +".");
 var seventhGraders = prompt("How many seventh graders?");
-console.log(seventhGraders);
+console.log("Number of seventh graders :" + seventhGraders +".");
 var eigthGraders = prompt("How many eigth graders?");
-console.log(eigthGraders);
+console.log("Number of eigth graders :"+ eigthGraders +".");
 var ninthGraders = prompt("How many ninth graders?");
-console.log(ninthGraders);
+console.log("Number of ninth graders :"+ ninthGraders +".");
 var tenthGraders =prompt("How many tenth graders?");
-console.log(tenthGraders);
+console.log("Number of tenth graders :"+ tenthGraders +".");
 var eleventhGraders = prompt("How many eleventh graders?");
-console.log(eleventhGraders);
-var twelthGraders = prompt("How many twelth graders?");
-console.log(twelthGraders);
+console.log("Number of eleventh graders :"+ eleventhGraders +".");
+var twelfthGraders = prompt("How many twelth graders?");
+console.log("Number of twelth graders :"+ twelfthGraders +".");
 
 //finding the total number of students and printing the total to the console
-var grades = [Number(firstGraders), Number(secondGraders), Number(thirdGraders), Number(fourthGraders), Number(fifthGraders), Number(sixthGraders), Number(seventhGraders), Number(eigthGraders), Number(ninthGraders), Number(tenthGraders), Number(eleventhGraders), Number(twelthGraders)];
+var grades = [Number(firstGraders), Number(secondGraders), Number(thirdGraders), Number(fourthGraders), Number(fifthGraders), Number(sixthGraders), Number(seventhGraders), Number(eigthGraders), Number(ninthGraders), Number(tenthGraders), Number(eleventhGraders), Number(twelfthGraders)];
 var totalStudents = grades[0] + grades[1] + grades[2] + grades[3] + grades[4] + grades[5] + grades[6] + grades[7] + grades[8] + grades[9] + grades[10] + grades[11];
 console.log("You have "+ totalStudents +" total students.");
 
@@ -54,18 +55,18 @@ alert("You will spend a total of $" + totalSpent +" on Easter Baskets for your s
 
 // It says to comment out test values I used 1 for all grades
 //I copied this from the console
-//1 script.js:11:1   It shows the line that the console.log was on
-//1 script.js:13:1
-//1 script.js:15:1
-//1 script.js:17:1
-//1 script.js:19:1
-//1 script.js:21:1
-//1 script.js:23:1
-//1 script.js:25:1
-//1 script.js:27:1
-//1 script.js:29:1
-//1 script.js:31:1
-//1 script.js:33:1   all lines for grades were correct with 1
+//1 script.js:11:Number of first graders: 1.   It shows the line that the console.log was on
+//1 script.js:13:Number of second graders: 1.
+//1 script.js:15:Number of third graders: 1.
+//1 script.js:17:Number of fourth graders: 1.
+//1 script.js:19:Number of fifth graders: 1.
+//1 script.js:21:Number of sixth graders: 1.
+//1 script.js:23:Number of seventh graders: 1.
+//1 script.js:25:Number of eigth graders: 1.
+//1 script.js:27:Number of ninth graders: 1.
+//1 script.js:29:Number of tenth graders: 1.
+//1 script.js:31:Number of eleventh graders: 1.
+//1 script.js:33:Number of twelfth graders: 1.  all lines for grades were correct with 1
 //You have 12 total students. script.js:38:1    there are 12 students so this is correct
 //You want to spend $15 per student. script.js:45:1    $15 per Student so this is correct
 //You will spend a total of $180 on your students Easter Baskets.   This is also correct
