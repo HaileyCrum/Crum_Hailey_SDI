e0ac1b6bb7da415447ed32dabb203dad833b0413
hdcrum <hailey@Haileys-MacBook-Pro.local>
2015-09-10 23:48:34 -0400

    Last final touches
    Corrected more spelling and added last missing elements

--- Crum_Hailey_Expression_Assignment/JS/script.js
+++ Crum_Hailey_Expression_Assignment/JS/script.js
@@ -6,35 +6,56 @@ Expression Assignment
 //Alerting the user that I want to help them calculate how much it will cost to buy easter baskets for every student
 alert("Hello, I am going to help you find out how much it will cost for all the students in school to get an easter basket.");
 
-//Prompting the user how many students are in each grade and printing it out in the console
+//Prompting the user how many students are in first grade and printing it out in the console
 var firstGraders =prompt("How many first graders?");
-console.log("Number of first graders "+
-firstGraders +".");
+console.log("Number of first graders :"+ firstGraders +".");
+
+//Prompting the user how many students are in  second grade and printing it out in the console
 var secondGraders =prompt("How many second graders?");
-console.log("Number of Second graders :"+ secondGraders +".");
+console.log("Number of second graders :"+ secondGraders +".");
+
+//Prompting the user how many students are in third grade and printing it out in the console
 var thirdGraders = prompt("How many third graders?");
 console.log("Number of third graders :"+ thirdGraders +".");
+
+//Prompting the user how many students are in fourth grade and printing it out in the console
 var fourthGraders =prompt("How many fourth graders?");
 console.log("Number of fourth graders :"+ fourthGraders +".");
+
+//Prompting the user how many students are in fifth grade and printing it out in the console
 var fifthGraders = prompt("How many fifth graders?");
 console.log("Number of fifth graders :"+ fifthGraders +".");
+
+//Prompting the user how many students are in sixth grade and printing it out in the console
 var sixthGraders = prompt("How many sixth graders?");
 console.log("Number of sixth graders :"+ sixthGraders +".");
+
+//Prompting the user how many students are in seventh grade and printing it out in the console
 var seventhGraders = prompt("How many seventh graders?");
 console.log("Number of seventh graders :" + seventhGraders +".");
-var eigthGraders = prompt("How many eigth graders?");
-console.log("Number of eigth graders :"+ eigthGraders +".");
+
+//Prompting the user how many students are in eighth grade and printing it out in the console
+var eighthGraders = prompt("How many eigth graders?");
+console.log("Number of eigth graders :"+ eighthGraders +".");
+
+//Prompting the user how many students are in ninth grade and printing it out in the console
 var ninthGraders = prompt("How many ninth graders?");
 console.log("Number of ninth graders :"+ ninthGraders +".");
+
+//Prompting the user how many students are in tenth grade and printing it out in the console
 var tenthGraders =prompt("How many tenth graders?");
 console.log("Number of tenth graders :"+ tenthGraders +".");
+
+//Prompting the user how many students are in eleventh grade and printing it out in the console
 var eleventhGraders = prompt("How many eleventh graders?");
 console.log("Number of eleventh graders :"+ eleventhGraders +".");
+
+//Prompting the user how many students are in twelfth grade and printing it out in the console
 var twelfthGraders = prompt("How many twelth graders?");
 console.log("Number of twelth graders :"+ twelfthGraders +".");
 
 //finding the total number of students and printing the total to the console
-var grades = [Number(firstGraders), Number(secondGraders), Number(thirdGraders), Number(fourthGraders), Number(fifthGraders), Number(sixthGraders), Number(seventhGraders), Number(eigthGraders), Number(ninthGraders), Number(tenthGraders), Number(eleventhGraders), Number(twelfthGraders)];
+var grades = [Number(firstGraders), Number(secondGraders), Number(thirdGraders), Number(fourthGraders), Number(fifthGraders), Number(sixthGraders), Number(seventhGraders), Number(eighthGraders), Number(ninthGraders), Number(tenthGraders), Number(eleventhGraders), Number(twelfthGraders)];
 var totalStudents = grades[0] + grades[1] + grades[2] + grades[3] + grades[4] + grades[5] + grades[6] + grades[7] + grades[8] + grades[9] + grades[10] + grades[11];
 console.log("You have "+ totalStudents +" total students.");
 
@@ -62,7 +83,7 @@ alert("You will spend a total of $" + totalSpent +" on Easter Baskets for your s
 //1 script.js:19:Number of fifth graders: 1.
 //1 script.js:21:Number of sixth graders: 1.
 //1 script.js:23:Number of seventh graders: 1.
-//1 script.js:25:Number of eigth graders: 1.
+//1 script.js:25:Number of eighth graders: 1.
 //1 script.js:27:Number of ninth graders: 1.
 //1 script.js:29:Number of tenth graders: 1.
 //1 script.js:31:Number of eleventh graders: 1.
