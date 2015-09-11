c4130e6502453c28dc6bf66fb8cfb777b04c28bf
hdcrum <hailey@Haileys-MacBook-Pro.local>
2015-09-10 22:48:43 -0400

    working on assignment
    Trying to figure out idea.

--- Crum_Hailey_Expression_Assignment/JS/script.js
+++ Crum_Hailey_Expression_Assignment/JS/script.js
@@ -4,3 +4,38 @@ Expression Assignment
 09/10/2015
  */
 
+alert("Hello, I am going to help you find out how much it will cost for all the students in school to get an easter basket.");
+
+var firstGraders =prompt("How many first graders?");
+console.log(firstGraders);
+var secondGraders =prompt("How many second graders?");
+console.log(secondGraders);
+var thirdGraders = prompt("How many third graders?");
+console.log(thirdGraders);
+var fourthGraders =prompt("How many fourth graders?");
+console.log(fourthGraders);
+var fifthGraders = prompt("How many fifth graders?");
+console.log(fifthGraders);
+var sixthGraders = prompt("How many sixth graders?");
+console.log(sixthGraders);
+var seventhGraders = prompt("How many seventh graders?");
+console.log(seventhGraders);
+var eigthGraders = prompt("How many eigth graders?");
+console.log(eigthGraders);
+var ninthGraders = prompt("How many ninth graders?");
+console.log(ninthGraders);
+var tenthGraders =prompt("How many tenth graders?");
+console.log(tenthGraders);
+var eleventhGraders = prompt("How many eleventh graders?");
+console.log(eleventhGraders);
+var twelthGraders = prompt("How many twelth graders?");
+console.log(twelthGraders);
+
+
+var totalStudents = Number(firstGraders) + Number(secondGraders) + Number(thirdGraders) + Number(fourthGraders) + Number(fifthGraders) + Number(sixthGraders) + Number(seventhGraders) + Number(eigthGraders) + Number(ninthGraders) + Number(tenthGraders) + Number(eleventhGraders) + Number(twelthGraders);
+console.log(totalStudents);
+Alert("You have "+ totalStudents +"total studens.");
+
+var perStudent = prompt("How much do you want to spend per person?");
+console.log("$" + perStudent +);
+console.log 
