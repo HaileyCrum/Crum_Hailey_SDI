5509c8c5856fea694c005e2d6c70e4155df3394e
hdcrum <hailey@Haileys-MacBook-Pro.local>
2015-09-18 00:02:02 -0400

    Conditionals Assignment: Turned in assignment
    I am going to continue to work on this assignment and finish it. I
    missed a lot of elements in the code.

--- Crum_Hailey_Conditionals_Assignments/JS/script.js
+++ Crum_Hailey_Conditionals_Assignments/JS/script.js
@@ -4,55 +4,7 @@ Hailey Crum
 Conditionals Assignment
 9/17/15
  */
-
- Alert("Hello, I'm gonna ask you 2 questions! Please answer them all.");
-
- var q1 = prompt("What is the month of your birthday?");
- if(q1 ===""){
-     name = prompt("Please do not leave blank, What is the month of your birthday?")
- }
- var q2 = prompt("What is the date of your birthday?");
- console.log(isNaN(q1)); //true
- var false = console.log(isNaN(q2)); //returns false
- if(false === "false"){
-     q1 = prompt("Please enter a number between 1-31.");
- }
-
-
-if( q1 === "January" && q2 <= 19;){
-    console.log("You are a capricorn");
-    alert("");
-} else if(){
-    console.log("");
-    alert("");
-} else if(""){
-     console.log("");
-     alert("");
- }else if(""){
-    console.log("");
-    alert("");
-} else{
-    console.log("");
-    alert("");
-}
-
-/*VALIDATING prompt
-  if(name ===""){
-  //The User left it blank
-  //reprompt the user using the SAME variable
-  name = prompt("Please do not leave blank.\n What is your name?");
-  }
-  VALIDATING NUMBERS
-  var num1 = prompt("Please type in a number");
-
-  //Test to see if it is a number
-
-  //isNaN(it to test)
-  console.log(isNaN("cat")); //true
-  console.log(isNaN(7)); //returns false
-*/
-
-*/
+ 
 
 // 1 USD = .7375 EUR
  //1 US Dollar equals 16.58 Mexican Peso
