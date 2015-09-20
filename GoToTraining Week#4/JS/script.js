774dcb35a7ae0e9ad2b75f0fa482c7aff7c66abd
hdcrum <hailey@Haileys-MacBook-Pro.local>
2015-09-20 16:57:25 -0400

    Prompted for a min Number
    Validated for blank and not a number using a while loop.

--- GoToTraining Week#4/JS/script.js
+++ GoToTraining Week#4/JS/script.js
@@ -6,3 +6,37 @@ GoToTraining Week #4
  */
 
 //alert("Testing to see if connected!");
+
+ //Create a file that will give us a random number between two numbers
+
+
+//Ask the user for the minimun number
+ var minNum =  prompt("Let's find a number between two numbers! \n  What is the minimun number?");
+
+ //parse int the variable
+// minNum= parseInt(minNum);
+
+ while(minNum === "" || isNaN(minNum)){
+     //Reprompt the user
+     if(minNum ===""){
+         minNum = prompt("Please do not leave blank. \n What is the minimun number?");
+     }else if(isNaN(minNum)){
+         minNum = prompt("Please only use numbers. \n What is the minimun number?");
+        // minNum=parseInt(minNum);
+     }
+ }
+
+
+
+
+ /*
+ Basic Structure of a While loop
+
+ //Declare a variable
+ var num = prompt("Type in a number");
+ while(condition to test){
+    //Some sort of change to the variable in our condition to test.
+    X++;
+
+ }
+  */
